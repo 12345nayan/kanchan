a3882b0126ec46d5b8b155ec62fea424ebd20fe7
12345nayan <nayanrsonavane1997@gmail.com>
2018-11-30 20:01:05 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+From python:3
+
+ADD arithmatic.py /
+CMD ["python","./arithmatic.py"]
+
+
+
+
---
+++ arithmatic.py
@@ -0,0 +1,17 @@
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print ("Line 1 - Value of c is ", c)
+
+c = a - b
+print ("Line 2 - Value of c is ", c) 
+
+c = a * b
+print ("Line 3 - Value of c is ", c) 
+
+c = a / b
+print ("Line 4 - Value of c is ", c)
+
+
